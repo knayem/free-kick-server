44bd8c3a8a5d7116b768d0bf4846095c6727a821
knayem <abs99knayem@gmail.com>
2022-03-24 18:11:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app= express();
+const port= process.env.PORT || 5000;
+
+app.get('/',(req, res) =>{
+
+
+    res.send('free-kick-server is running')
+});
+
+app.listen(port, () =>{
+console.log('server is running at port',port);
+
+})
